60b2604f79eafd2b4d076137f4cd4cb5ad4bedbf
MusaSudad <musasudad271@gmail.com>
2021-12-19 22:33:47 +0600

    guessing game - mistakes covered

--- Excercises/Scratchpad/Musa/w3resources3.py
+++ Excercises/Scratchpad/Musa/w3resources3.py
@@ -1,14 +1,13 @@
-# Please add question in the file so that others who are reading gets a context
-# Please move the Files Under Appropriate folder (Come up with folder structure {may be Excercises/ Scratchpad/ Musa})
+#Asking the user to type a number
+guessing = int(input("Guess a number:"))
+#having a loop so that if the user types any other thing the code re-runs
+while guessing>= 10:
+    guessing = int(input("Guess a number:"))
+#if statements so that
+if (guessing)<1 or int(guessing)>9:
+    guessing = input("Guess a number:")
 
-guessnum = input("Guess a number:")
-# Variable name should be more meaningful, it makes code readable, Python should read like plain English
-
-if int(guessnum)<1 or int(guessnum)>9:
-    # instead of parsing 4 times to int, it will worth parsing once before, it will make the code efficient
-    guessnum = input("Guess a number:")
-
-elif int(guessnum)>=1 and int(guessnum)<=9:
+elif (guessing)>=1 and (guessing)<=9:
     print("Well guessed!")
 
 
