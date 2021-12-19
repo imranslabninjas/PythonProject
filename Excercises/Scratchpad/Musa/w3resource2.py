f1b2d46a76b68ccf9b9cdc4cbdcc930e076555e8
MusaSudad <musasudad271@gmail.com>
2021-12-19 23:00:17 +0600

    temperature converter - mistakes covered

--- Excercises/Scratchpad/Musa/w3resource2.py
+++ Excercises/Scratchpad/Musa/w3resource2.py
@@ -1,23 +1,15 @@
-# Please add question in the file so that others who are reading gets a context
-# Please move the Files Under Appropriate folder (Come up with folder structure {may be Excercises/ Scratchpad/ Zarif})
+#Asking user to input a temperatre value
+temp= int(input("What' the temperature:"))
+#Asking user in what to convert the temperature in.
+converter=str(input("Do you want to convert it into c for Celcius or in f for Farenheit\n"))
+#Using if statements  to convert value
+if converter == "f" or "F":
+    print((5/9) * (temp - 32))
+elif converter == "c" or "C":
+    print((temp*9/5)+32)
 
-temp = input("What's the temp\n")
 
-# Do you know what is the meaning of \n used above? worth a google search
-# Please put your favorite article as a comment for future reader
 
-con1 = input("Convert into C or celcius and F for farenheit\n")
-# Lets try to name the variable names appropriately. con1 does not mean anything. You can write temperature or ~
 
-con2 = 0
-if str(con1) == "C":
-    # What will happen when the user will put c with the smaller letter
-    con2 = int(temp) - 32* 5 / 9
 
-elif str(con1) == "F":
-    # What if the user pressee F with a dot, how do you validate? - food for thought.
-    con2 = int(temp) * 9 / 5+32
-
-    # What about when the user enters anything else like "Moon"
-print(con2)
 
