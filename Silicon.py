d4449814f855d31e34e1e5a9f969f0135dc8ee98
Zarif <zarifabdullah033@gmail.com>
2021-12-19 11:02:28 +0600

    2nd Task W3r

---
+++ Silicon.py
@@ -0,0 +1,9 @@
+
+
+Detector= int(input("Please input a temperature in Celcius: "))
+fahrenheit_converter= (9*Detector+(32*5))/5
+print(f"The temperature{Detector}°C is {fahrenheit_converter}°F")
+
+
+
+
