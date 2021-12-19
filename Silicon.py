ab4a8cdf5e92ddeb6c1dbf724083f0a30258f61a
Zarif <zarifabdullah033@gmail.com>
2021-12-19 23:55:13 +0600

    2nd Task W3r

--- Silicon.py
+++ Silicon.py
@@ -1,3 +1,20 @@
-C= int(input("Please input the temp in Celcius: "))
-F= (9*C+(32*5))/5
-print(f"The {C}°C is {F}°F")
+
+
+
+temp= input("Please give the temperature you want to convert: ")
+degree= int(temp[:-1])
+ai= temp[-1]
+C= (5/9) * (degree - 32)
+F=(9*degree + (32*5))/5
+
+if ai.upper()=="C":
+     print(f"The {degree}°C is {F:.2f}°F")
+
+elif ai.upper()=="F":
+     print (f"The {degree}°F is {F:.2f}°C")
+
+
+
+
+
+
