34d7ad45080060a3d9d80d8366fe8cf71d5c3445
ZarifAbdullah <89939932+ZarifAbdullah@users.noreply.github.com>
2021-12-19 11:08:35 +0600

    Delete ExcercisesScratchPadZarifSilicon.py

--- ExcercisesScratchPadZarifSilicon.py
+++
@@ -1,9 +0,0 @@
-
-
-Detector= int(input("Please input a temperature in Celcius: "))
-fahrenheit_converter= (9*Detector+(32*5))/5
-print(f"The temperature{Detector}°C is {fahrenheit_converter}°F")
-
-
-
-
