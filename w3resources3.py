1374271f71fe44e96ecda82e04e192285dc6cb77
Islam, Imranul <imranul.islam@oracle.com>
2021-12-18 20:44:49 -0500

    Added some discussion notes, it will help learning

--- w3resources3.py
+++ w3resources3.py
@@ -2,8 +2,12 @@
 # Please move the Files Under Appropriate folder (Come up with folder structure {may be Excercises/ Scratchpad/ Musa})
 
 guessnum = input("Guess a number:")
+# Variable name should be more meaningful, it makes code readable, Python should read like plain English
+
 if int(guessnum)<1 or int(guessnum)>9:
+    # instead of parsing 4 times to int, it will worth parsing once before, it will make the code efficient
     guessnum = input("Guess a number:")
+
 elif int(guessnum)>=1 and int(guessnum)<=9:
     print("Well guessed!")
 
