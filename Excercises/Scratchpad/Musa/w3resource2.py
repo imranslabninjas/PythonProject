dea27117d9c4d7226a973c7b36bd8726fbcffbbb
MusaSudad <musasudad271@gmail.com>
2021-12-20 11:35:39 +0600

    temperature converter - mistakes covered

--- Excercises/Scratchpad/Musa/w3resource2.py
+++ Excercises/Scratchpad/Musa/w3resource2.py
@@ -3,14 +3,6 @@ temperature = int(input("What's the temperature:"))
 #asking in what to convert
 converter= str(input("Do you want to convert it into c for celcius or f for farenheit:"))
 if converter=="c" or converter=="C":
-    print((temperature-32) * 5/9)
+    print(int((temperature-32) * 5/9),"°F")
 elif converter == "F" or converter=="f":
-    print((temperature * 9/5) + 32)
-#adding loop so that if user mistypes code re-runs
-while converter!= "f" or converter!="F" or converter!="c" or converter !="C":
-    temperature = int(input("What's the temperature:"))
-    converter = str(input("Do you want to convert it into c for celcius or f for farenheit\n(PLEASE TYPE C IF YOU WANT TO CONVERT IT INTO CELCIUS AND F FOR FARENHEIT)->"))
-    if converter == "c" or converter == "C":
-        print((temperature - 32) * 5 / 9)
-    elif converter == "F" or converter == "f":
-        print((temperature * 9 / 5) + 32)
+    print(int((temperature * 9/5) + 32),"°C")
