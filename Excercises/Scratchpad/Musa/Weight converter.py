f90614e9636f4f1a83a8e8f97e40cdd9f7087fc3
MusaSudad <86278155+MusaSudad@users.noreply.github.com>
2021-12-24 22:22:41 +0600

    Delete Weight converter.py

--- Excercises/Scratchpad/Musa/Weight converter.py
+++
@@ -1,8 +0,0 @@
-weight=int(input("Weight:"))
-unit=input("(K)g or (L)bs:")
-if unit=="L" or "l":
-    converted = weight*2.205
-    print("Weight in Lbs:"+str(converted))
-elif unit=="K" or "k":
-    converted = weight*0.45
-    print("Weight in Kgs:"+str(converted))
