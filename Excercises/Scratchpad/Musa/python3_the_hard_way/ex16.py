712860de88d53b4a22a7d6b43367c34b0cadde52
MusaSudad <musasudad271@gmail.com>
2021-12-28 21:38:44 +0600

    updated

--- Excercises/Scratchpad/Musa/python3_the_hard_way/ex16.py
+++ Excercises/Scratchpad/Musa/python3_the_hard_way/ex16.py
@@ -1,4 +1,26 @@
 from sys import argv
+script,codefile=argv
+print(f"Hey there I am {script}")
+print(f"Do you want me to delete {codefile}")
+asking=input("Yes or no")
+if asking=="Yes"or"yes":
+    opener=open(codefile,'w')
+    opener.truncate()
+    print("Done*_*")
+elif asking=="No"or"no":
+    print("Okay:)")
 
+asking=(input(f"Do you want to write in {codefile}"))
+
+if asking=="Yes"or"yes":
+    opener=open(codefile,"w")
+    line1=input(">")
+    line2=input(">")
+    line3=input(">")
+    opener.write(line1)
+    opener.write(line2)
+    opener.write(line3)
+    print("Done0_o")
+    opener.close()
 
 
